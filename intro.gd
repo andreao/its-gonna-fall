929ecc032ca441ca94c700bdcee5f86c17376155
Andreas <andreas.oppeboen@gmail.com>
2021-10-21 17:00:52 +0200

    Reorganize code into more logical directories

--- intro.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _on_play_button_pressed():
-	get_tree().change_scene("res://main_scene.tscn")
---
+++ intro/intro.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _on_play_button_pressed():
+	get_tree().change_scene("res://main/main_scene.tscn")
